4c91033fd4cfe32d689fe0f6fd97cd943fc5c8ca
jusebapo <juan.barreroportes5@gmail.com>
2023-05-09 01:12:19 -0500

    Avances Inicio Plataforma

--- Usuario.php
+++ Usuario.php
@@ -49,7 +49,7 @@ class Usuario
 
 
         $passwordCif = md5($password);
-        echo " $passwordCif ";
+        // echo " $passwordCif ";
         // Preparar la consulta SQL con sentencias preparadas
         $consulta = "SELECT nickUsu, pasUsu FROM usuarios WHERE nickUSu = ? AND pasUsu = ?";
         $stmt = mysqli_prepare($link, $consulta);
@@ -66,7 +66,7 @@ class Usuario
 
         // Devolver verdadero si hubo al menos un resultado, falso en caso contrario
         if ($num_resultados > 0) {
-           /*  header("Location: Plataforma.html"); */
+            header("Location: Plataforma.html");
             $link2 = mysqli_connect($this->servidor, $this->usuario, "", $this->basedato);
 
             mysqli_select_db($link2, $this->basedato);
@@ -75,9 +75,9 @@ class Usuario
             $horaIngreso = date("H:i:s");
 
 
-            $consultaId = "SELECT idUsuarios FROM usuarios WHERE nickUsu = ?";
+            $consultaId = "SELECT idUsuario FROM usuarios WHERE nickUsu = ?";
             $stmt2 = mysqli_prepare($link2, $consultaId);
-            mysqli_stmt_bind_param($stmt2, "s" , $usuariop);
+            mysqli_stmt_bind_param($stmt2, "s", $usuariop);
 
             mysqli_stmt_execute($stmt2);
 
@@ -88,12 +88,12 @@ class Usuario
             mysqli_stmt_close($stmt2);
 
 
-            if ($num_resultados2 > 0){
+            if ($num_resultados2 > 0) {
                 $fila = mysqli_fetch_assoc($resultado2);
-                $idUsuario = $fila['idUsuarios'];
+                $idUsuario = $fila['idUsuario'];
             }
 
-            
+
             echo " $fechaIngreso , $horaIngreso , $idUsuario ";
 
             $grabar_bitacora = "INSERT INTO bitacora(fechaIngreso,horaIngreso,idUsuario) VALUES( '$fechaIngreso', '$horaIngreso' , $idUsuario)"; //guardo los datos
@@ -101,7 +101,7 @@ class Usuario
 
             $guardar_bitacora = mysqli_query($link2, $grabar_bitacora) or die('El registro de datos fall&oacute;: ' . mysqli_connect_error());
 
-            
+
             mysqli_close($link2);
             exit;
         } else {
